56f8f2c2d42632631129dfcd3b37c720496e54ac
Adarsh Goswami <adarsh.scnetwork@gmail.com>
2025-11-10 01:07:10 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, num + 1):
+            result *= i
+        return result
+
+number = int(input("Enter a number: "))
+print(f"Factorial of {number} is: {factorial(number)}")
